bb55ef11686a27e4aa5d8b7a0a199a4817aa63e7
viktortodorov55 <vtodorov@alumnes.ilerna.com>
2025-11-03 19:44:38 +0100

    Viktor: He añadido cambios al programa

--- Holagithub.java
+++ Holagithub.java
@@ -3,9 +3,9 @@ public class Holagithub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Hola buenas soy Viktor y estoy haciendo un trabajo de Entornos:)
+	Hola buenas soy Viktor y estoy haciendo el trabajo de Entornos:)
 
-	System.out.println("ey hola MARCEL");
+
 	}
 
 }

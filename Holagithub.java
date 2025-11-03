93053dbc14f26c6cf5eb516d15e928ed4e7251c1
Marcel <marcel.codiperpi@alumnes.ilerna.com>
2025-11-03 19:57:42 +0100

    Marcel: eh quitado cosas del programa

--- Holagithub.java
+++ Holagithub.java
@@ -2,10 +2,11 @@
 public class Holagithub {
 
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
-		Hola buenas soy Viktor y estoy haciendo un trabajo de Entornos:)
+		
 
-	System.out.println("ey hola MARCEL");
-	}
+ 		HEY HOLA,  VERISON BUENA.
 
+  }
 }
